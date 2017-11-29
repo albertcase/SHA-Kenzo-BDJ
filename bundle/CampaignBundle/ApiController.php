1f7c7f015ca4e0fe8a77a60dc2134f10aceede14
ke.wang@samesamechina.com <ke.wang@samesamechina.com>
2017-11-29 21:09:37 +0800

    en:add ajax check and check phone code

--- bundle/CampaignBundle/ApiController.php
+++ bundle/CampaignBundle/ApiController.php
@@ -22,9 +22,13 @@ class ApiController extends Controller
 
         parent::__construct();
 
-        // if(!$user->uid) {
-        //     $this->statusPrint('100', 'access deny!');
-        // }
+        // ajax 请求的处理方式 
+        if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"])=="xmlhttprequest"){ 
+            //@TODO ? 
+        } else{ 
+            $this->statusPrint('100', 'access deny!');
+        }
+
         $this->_pdo = PDO::getInstance();
         $this->helper = new Helper();
     }
---
+++ script/check_phonecode.php
@@ -0,0 +1,13 @@
+<?php
+define('SITE_URL', dirname(dirname(__FILE__)));
+require_once SITE_URL . "/vendor/autoload.php";
+require_once SITE_URL . "/config/config.php";
+
+use Lib\Redis;
+
+$phone = $argv[1];
+
+$redis = new Redis();
+var_dump($redis->get($phone));
+
+exit;
